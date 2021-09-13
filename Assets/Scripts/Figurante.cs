a4b02fa4450659a549d16a5e4b06727ce61034ae
guilhermecpp <guilhermecdemenezes123@gmail.com>
2021-09-12 22:11:52 -0300

    Primeira e segunda fase funcionando

---
+++ Assets/Scripts/Figurante.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Figurante : MonoBehaviour
+{
+
+  void Update()
+  {
+      transform.Translate(Vector2.right * 1.5f * Time.deltaTime);
+  }
+
+}
